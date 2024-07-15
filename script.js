2f3eb68dd5cd9085696b8c3043ed197ebc141d61
AlexJoelMontano <alexjoelmontano@gmail.com>
2024-07-15 11:50:06 -0400

    sign-up form js will switch the pswd no match error depending on focus proper email validation to ensure a correct email is entered

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-const input = document.querySelector("input");
-const initPass = document.querySelector("#inputPass");
-const validPass = document.querySelector("#validPass");
+let input = document.querySelectorAll("input");
+let initPass = document.querySelector("#inputPass");
+let validPass = document.querySelector("#validPass");
 let passTips = document.querySelector("#passTips");
 let passwordLabels = document.querySelectorAll(".check");
+let message = document.querySelector("#message");
+let message2 = document.querySelector("#message2");
 
 let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
 initPass.addEventListener("focus", () =>{
-    passTips.style.display = "block";
+    passTips.style.display = "flex";
+    passTips.style.flexDirection = "column";
+    passTips.style.height = "12em";
+    if (validPass.value != ""){
+      message2.style.display = "block";
+      message2.className = "invalid";
+      message.style.display = "none";
+      passTips.style.fontSize = "calc(0.36rem + 0.4vw)"
+      document.querySelector("#tipTitle").style.display = "none";
+    }
+    else{
+      message2.style.display = "none";
+    }
+
 })
 initPass.onblur = function(){
     passTips.style.display = "none";
+    message.style.position = "absolute";
 }
 let letter = document.querySelector("#letter");
 let capital = document.querySelector("#capital");
@@ -54,12 +70,28 @@ initPass.addEventListener("keyup", () =>{
 function check() {
   if (document.querySelector("#inputPass").value ==
   document.querySelector("#validPass").value) {
-      initPass.className = "valid";
-      validPass.className = "valid";
+      initPass.className = "text-dark";
+      validPass.className = "text-dark";
+      message.style.display = "none";
   
   } else {
       initPass.className = "invalid";
       validPass.className = "invalid";
+      message.style.display = "block";
+      message.className = "small-red";
+      message.textContent = "Passwords do not match";
   }
 }
 
+for (let inp of input){
+
+  inp.addEventListener("click", () =>{
+    if (inp.value == ""){
+      inp.setAttribute("required");
+      inp.className = "invalid";
+    }
+    else {
+      inp.removeAttribute("required");
+    }
+  })
+}
